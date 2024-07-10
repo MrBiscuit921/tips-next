281745fc410d6ad365528632812803042ea01a9f
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2024-07-10 23:16:19 +1200

    Add next auth and modify sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import Info from "@/components/Info";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,15 +13,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <SessionWrapper>
     <html lang="en">
       <body className={inter.className}>
         <div className="flex justify-between max-w-6xl mx-auto">
           <div className='hidden sm:inline border-r h-screen'>
             <Sidebar />
+
           </div>
-          <div>{children}</div>
+          <div className="w-2x1 flex-1">{children}</div>
+          <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+              <Info />
+            </div>
         </div>
       </body>
     </html>
+    </SessionWrapper>
   );
 }
---
+++ src/components/info.jsx
@@ -0,0 +1,5 @@
+export default function info() {
+  return (
+    <div>info</div>
+  )
+}
