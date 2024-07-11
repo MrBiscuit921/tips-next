cc7e1ea95194dbfa3571439f452c86342e22f42a
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2024-07-11 13:28:58 +1200

    test

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,7 +1,12 @@
-'use client';
-
 export default function Info() {
   return (
-    <div>Info</div>
+    <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2'>
+      <h4 className='font-bold text-xl px-4'>Whats happening</h4>
+      <button
+        className='text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm'>
+        Load more
+      </button>
+    </div>
   );
 }
+
