a95bd30bfe5f5fb9dcb43bebdcf5110450b3f617
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2024-07-12 21:37:46 +1200

    bug fix

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-export default function ads() {
+export default function Ads() {
     useEffect(() => {
         try {
           (window.adsbygoogle = window.adsbygoogle || []).push({});
